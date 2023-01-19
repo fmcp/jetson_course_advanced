5eabf703f30a3477e2796ccfb0d80c69d375be67
PaulaRuizB <pruizb@uma.es>
2023-01-19 13:39:48 +0100

    Create trt_quantization

---
+++ TensorRT/trt_quantization.sh
@@ -0,0 +1,17 @@
+#Quantization
+
+#FP32
+/usr/src/tensorrt/bin/trtexec --onnx=/home/nano/resnet50_onnx.onnx --saveEngine=/home/nano/trt_models/resnet50_batch1_fp32.trt
+#/usr/src/tensorrt/bin/trtexec --loadEngine=/home/nano/trt_models/resnet50_batch1_fp32.trt
+
+#FP16
+#/usr/src/tensorrt/bin/trtexec --onnx=/home/nano/resnet50_onnx.onnx --saveEngine=/home/nano/trt_models/resnet50_batch1_fp16.trt --fp16
+#/usr/src/tensorrt/bin/trtexec --loadEngine=/home/nano/trt_models/resnet50_batch1_fp16.trt --fp16
+
+#INT8
+#/usr/src/tensorrt/bin/trtexec --onnx=/home/nano/resnet50_onnx.onnx --saveEngine=/home/nano/trt_models/resnet50_batch1_int8.trt --int8
+#/usr/src/tensorrt/bin/trtexec --loadEngine=/home/nano/trt_models/resnet50_batch1_int8.trt --int8
+
+#Best
+#/usr/src/tensorrt/bin/trtexec --onnx=/home/nano/resnet50_onnx.onnx --saveEngine=/home/nano/trt_models/resnet50_batch1_best.trt --best
+#/usr/src/tensorrt/bin/trtexec --loadEngine=/home/nano/trt_models/resnet50_batch1_best.trt --best
