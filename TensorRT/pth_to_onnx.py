79ca066f6539a96faa97411cbd53f8ae89681122
PaulaRuizB <pruizb@uma.es>
2023-01-19 12:10:38 +0100

    pth_to_onnx update

--- TensorRT/pth_to_onnx.py
+++ TensorRT/pth_to_onnx.py
@@ -1,12 +1,20 @@
 from torchvision import models
 import torch
+import argparse
 
-#ResNet50 pretrained model
-model = models.resnet50(pretrained=True)
+if __name__ == '__main__':
+    # Input arguments
+    parser = argparse.ArgumentParser(description='Course pth model')
+    parser.add_argument('--onnx_path', type=str, default='', required=True, help="Path to save model")
 
-input = torch.randn(1, 3, 224, 224)
+    args = parser.parse_args()
 
-onnx_path = 'resnet50.onnx'
+    onnx_path = args.onnx_path
 
-torch.onnx.export(model, input, onnx_path, input_names=['input'],
-                  output_names=['output'], export_params=True)
+    #ResNet50 pretrained model
+    model = models.resnet50(pretrained=True)
+
+    input = torch.randn(1, 3, 224, 224)
+
+    torch.onnx.export(model, input, onnx_path, input_names=['input'],
+                      output_names=['output'], export_params=True)
